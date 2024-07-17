16376852a24902d9934b282fde928d88fd9a9fd5
sanchit-cell <70786565+sanchit-cell@users.noreply.github.com>
2024-07-17 16:11:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://task1-w74l.onrender.com/", // your frontend domain
+    origin: "https://task1-w74l.onrender.com", // your frontend domain
     credentials: true, // allows session cookies to be sent and received
   })
 );
