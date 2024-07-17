1cb9e888faa7ead64b691361b992af5e9e96a643
sanchit-cell <70786565+sanchit-cell@users.noreply.github.com>
2024-07-17 15:56:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const Session = require("cookie-session")
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("dotenv").config();
 const cors = require("cors");
+const fs = require('fs');
+const https = require('https');
 require("./models/User");
 require("./services/passport");
 
@@ -31,34 +34,55 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://task1-w74l.onrender.com", // your frontend domain
+    origin: "http://localhost:3000", // your frontend domain
     credentials: true, // allows session cookies to be sent and received
   })
 );
 
 app.use(bodyParser.json());
 
-app.use(
-  session({
-    secret: keys.cookieKey,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+// app.use(
+//   session({
+//     secret: keys.cookieKey,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       secure: true // ensures the cookie is only used over HTTPS
+//     }
+//   })
+// );
+app.use(Session({
+  name: 'session',
+  keys: [keys.cookieKey], // Keys to sign the cookie, you can use environment variables for better security
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(require("./routes/auth"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("frontend/build"));
 
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
+
+// const sslOptions = {
+//   key: fs.readFileSync('ssl/server.key'),
+//   cert: fs.readFileSync('ssl/server.cert'),
+  
+// };
+
+
+
+// const PORT = process.env.PORT || 5000;
+// https.createServer(sslOptions, app).listen(PORT, () => {
+//   console.log(`Server running on https://localhost:${PORT}`);
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started at Port : ${PORT}`));
